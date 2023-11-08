d9ad857dc1747a841cb6a0ff98bd31a9a1e965c9
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-11-08 18:33:56 -0500

    this is working

--- public/main.js
+++ public/main.js
@@ -1,94 +1,72 @@
-let locations = [];
-
 window.addEventListener('DOMContentLoaded', () => {
     const callList = document.getElementById('call-list');
+    const chartContainer = document.getElementById('chart-container');
 
-    // Fetch call data from the server
     fetch('/databasestuff')
         .then((response) => {
-            console.log(response); // Check the response from the server
             return response.json();
         })
         .then((data) => {
-            console.log(data); // Check the data received and ensure it is in valid JSON format
+            const recentCalls = data.slice(0, 20);
 
-            // Iterate over the array of call data and add each call to the list
-            data.forEach(call => {
-                // Create a new div element for each call
-                let item = document.createElement('DIV');
-                const latitude = parseFloat(call.latitude);
-                const longitude = parseFloat(call.longitude);
-                const location = { lat: latitude, lng: longitude };
-                locations.push(location);
-                // Set the div text content to the call data
-                item.textContent = `Call #${call.dispcallid}: ${call.dispcalltypedescr} - ${call.dispsubtypedescr}`;
+            recentCalls.forEach(call => {
+                const callItem = document.createElement('div');
+                callItem.classList.add('call-item');
+                callItem.innerHTML = `
+                    <h3>${call.address}</h3>
+                    <h3>Incident Number: ${call.incnum}</h3>
+                    <h3>Call Type: ${call.dispcalltypedescr}</h3>
+                    <h4>Call Description: ${call.dispsubtypedescr}</h4>
+                    <p>Date: ${call.datetimealarm}</p>
+                    <p>Narrative: ${call.NARR}</p>
+                `;
+                callList.appendChild(callItem);
+            });
 
-                // Create a link that will expand/collapse the call data
-                let toggleLink = document.createElement('A');
-                toggleLink.setAttribute('href', '#');
-                toggleLink.textContent = 'Show More';
+            const addressCount = {};
 
-                toggleLink.onclick = function () {
-                    let callData = document.createElement('DIV');
-                    console.log('call:', call); // Add this line for debugging
-                    callData.textContent = `Time: ${call.time} Address: ${call.address}, ${call.city}, ${call.state}`;
-                  
-                    // Create a div for the map
-                    let mapDiv = document.createElement('DIV');
-                    mapDiv.setAttribute('id', 'map');
-                  
-                    // Set the item to display "flex"
-                    item.style.display = 'flex';
-                  
-                    // If item contains callData, remove it
-                    if (item.contains(callData)) {
-                      item.removeChild(callData);
-                      toggleLink.textContent = 'Show More';
-                      // If item contains mapDiv, remove it
-                      if (item.contains(mapDiv)) {
-                        item.removeChild(mapDiv);
-                      }
-                    }
-                  };
+            recentCalls.forEach(call => {
+                const address = call.address;
+                if (addressCount.hasOwnProperty(address)) {
+                    addressCount[address]++;
+                } else {
+                    addressCount[address] = 1;
+                }
+            });
 
-                // Add the new call to the call list
-                item.appendChild(toggleLink);
-                callList.appendChild(item);
+            const sortedAddressCount = Object.entries(addressCount)
+                .sort((a, b) => b[1] - a[1])
+                .slice(0, 5);
+
+            const chartData = {
+                labels: sortedAddressCount.map(([address]) => address),
+                datasets: [{
+                    label: 'Most Visited Addresses',
+                    data: sortedAddressCount.map(([address, count]) => count),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            };
+
+            const chartOptions = {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        precision: 0, // Display whole numbers
+                    },
+                },
+            };
+
+            const chartCanvas = document.createElement('canvas');
+            chartContainer.appendChild(chartCanvas);
+
+            const chartContext = chartCanvas.getContext('2d');
+            const chart = new Chart(chartContext, {
+                type: 'bar',
+                data: chartData,
+                options: chartOptions
             });
         })
-        .catch((error) => {
-            console.error('Error fetching call data:', error);
-        });
-});
-
-// Call the callback function when the Google Maps API script is loaded
-
-
-
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: locations[0],
-      zoom: 8,
-    });
-  
-    // Create an array to store the markers
-    const markers = [];
-  
-    // Loop through all the locations and create a marker for each one
-    locations.forEach((location) => {
-      const marker = new google.maps.Marker({
-        position: location,
-        map: map,
-      });
-  
-      // Add the marker to the array
-      markers.push(marker);
-    });
-  
-    // Create a marker clusterer to group nearby markers
-    new MarkerClusterer(map, markers, {
-      imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
-  }
-  
+})
