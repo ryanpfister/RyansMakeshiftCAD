39018e458e8017f34363fd2fc8bd0f695716375f
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-11-08 20:07:01 -0500

    lol

--- public/main.js
+++ public/main.js
@@ -1,13 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
     const callList = document.getElementById('call-list');
     const chartContainer = document.getElementById('chart-container');
+    const viewMoreButton = document.getElementById('view-more');
+
+    let allCalls = []; // to store all the fetched calls
 
     fetch('/databasestuff')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            const recentCalls = data.slice(0, 50);
+            allCalls = data; // store all the fetched calls
+            const recentCalls = data.slice(0, 5); // display only the first 5 calls
 
             recentCalls.forEach(call => {
                 const callItem = document.createElement('div');
@@ -17,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     <h3>Call Description: ${call.dispsubtypedescr}</h4>
                     <h4>${call.address}</h3>
                     <h4>Incident Number: ${call.incnum}</h3>
-                    <p>Date: ${call.datetimealarm}</p>
+                    <p>Date: ${call.datetimealarm} </p>
                     <h4>Location Name: ${call.sitename}</h4>
                     <p>Narrative: ${call.NARR}</p>
                     <h4>Caller Name: ${call.CALLNAME}</h4>
@@ -27,56 +31,35 @@ window.addEventListener('DOMContentLoaded', () => {
                 callList.appendChild(callItem);
             });
 
-            const addressCount = {};
+            if (data.length > 5) {
+                viewMoreButton.style.display = 'block'; // display the "View More" button if there are more calls
+            }
 
-            recentCalls.forEach(call => {
-                const address = call.address;
+            viewMoreButton.addEventListener('click', () => {
+                const remainingCalls = data.slice(5); // get the remaining calls
+                remainingCalls.forEach(call => {
+                    const callItem = document.createElement('div');
+                    callItem.classList.add('call-item');
+                    callItem.innerHTML = `
+                        <h3>Call Type: ${call.dispcalltypedescr}</h3>
+                        <h3>Call Description: ${call.dispsubtypedescr}</h4>
+                        <h4>${call.address}</h3>
+                        <h4>Incident Number: ${call.incnum}</h3>
+                        <p>Date: ${call.datetimealarm}</p>
+                        <h4>Location Name: ${call.sitename}</h4>
+                        <p>Narrative: ${call.NARR}</p>
+                        <h4>Caller Name: ${call.CALLNAME}</h4>
+                        <p>Caller Phone: ${call.CALLPHONE}</p>
+                        <p>Caller Address: ${call.CALLADDR}</p>
+                    `;
+                    callList.appendChild(callItem);
+                });
 
-                if (addressCount.hasOwnProperty(address)) {
-                    addressCount[address]++;
-                } else {
-                    addressCount[address] = 1;
-                }
-            });
-
-            const sortedAddressCount = Object.entries(addressCount)
-                .sort((a, b) => b[1] - a[1])
-                .slice(0, 5);
-
-            const chartData = {
-                labels: sortedAddressCount.map(([address]) => address),
-                datasets: [
-                    {
-                        label: 'Most Visited Addresses',
-                        data: sortedAddressCount.map(([address, count]) => count),
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            };
-
-            const chartOptions = {
-                responsive: true,
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        precision: 0
-                    }
-                }
-            };
-
-            const chartCanvas = document.createElement('canvas');
-            chartContainer.appendChild(chartCanvas);
-
-            const chartContext = chartCanvas.getContext('2d');
-            const chart = new Chart(chartContext, {
-                type: 'bar',
-                data: chartData,
-                options: chartOptions
+                viewMoreButton.style.display = 'none'; // hide the "View More" button after showing all the calls
             });
         })
         .catch((error) => {
             console.log('Error fetching data:', error);
         });
 });
+
