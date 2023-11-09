1db48c92281dfd9860c0d1af875956c8f1d80ec6
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-11-09 10:50:38 -0500

    Cache control

--- index.js
+++ index.js
@@ -265,6 +265,9 @@ app.use(function (req, res, next) {
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
+    res.setHeader('Cache-Control', 'no-cache, no-store');
+    res.setHeader('Expires', '0');
+    res.setHeader('Pragma', 'no-cache');
     // Pass to next layer of middleware
     next();
 });
