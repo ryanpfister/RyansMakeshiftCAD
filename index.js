2a14140d8ac73a7231ec750dfe422ca1fd46bac5
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-11-30 18:40:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -255,12 +255,20 @@ app.get('/callstodate', (req, res) => {
 
 // Ensure fullLatestCallID is a string before using substring
 if (typeof fullLatestCallID === 'string') {
+    // Extract the year and remove leading zeros
     const year = fullLatestCallID.substring(0, 4);
-    const callNumber = fullLatestCallID.substring(4).replace(/^0+/, '');
-    // Rest of your code handling the values
+    
+    // Ensure there is a substring to operate on
+    if (fullLatestCallID.length >= 5) {
+        const callNumber = fullLatestCallID.substring(4).replace(/^0+/, '');
+        // Rest of your code handling the values
+    } else {
+        console.error('Error: fullLatestCallID is too short to extract callNumber');
+    }
 } else {
     console.error('Error: fullLatestCallID is not a string');
 }
+
 
     // Return the modified result in JSON format
     res.json({ callNumber });
